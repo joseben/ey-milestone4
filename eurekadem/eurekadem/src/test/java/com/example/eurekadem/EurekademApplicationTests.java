59e70a2934faaf4adf696ba90504d8085b3e5b1c
Jose Ben <36511657+joseben@users.noreply.github.com>
2024-04-23 15:13:06 +0530

    update

---
+++ eurekadem/eurekadem/src/test/java/com/example/eurekadem/EurekademApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekadem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekademApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
